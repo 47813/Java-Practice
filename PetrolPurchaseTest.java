23f9c6f3f5103312cbc470bd7512a3da172f2346
이동언 <80028638+47813@users.noreply.github.com>
2022-11-29 22:51:04 +0900

    Delete PetrolPurchaseTest.java
    Deleted PetrolPurchaseTest.java due to making a complete file of Account. The reason is to make the repository more clear and simple.

--- PetrolPurchaseTest.java
+++
@@ -1,9 +0,0 @@
-public class PetrolPurchaseTest {
-    public static void main(String[] args) {
-        
-        PetrolPurchase info1 = new PetrolPurchase ("Location", "Type", 15, 5.00, 20.00);
-        
-        System.out.print("The total amount is ");
-        System.out.printf("%f", info1)
-    }
-}
